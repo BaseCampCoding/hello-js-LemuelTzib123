e810296010665ef91f9b9916873dae5a63c31d94
LemuelTzib123 <Lemueltzib@gmail.com>
2025-10-03 11:10:39 -0700

    fixed array

--- 5-Arrays/jasmine/src/main.js
+++ 5-Arrays/jasmine/src/main.js
@@ -1,41 +1,43 @@
+// Task 1
 let stack = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//Task 1
 let position = 5;
 let getItemResult = stack[position];
-console.log(getItemResult)
-//Task 2
-
+console.log(getItemResult);
+// Task 2
 position = 6;
-replacementCard = 17; 
+let replacementCard = 17;
 stack[position] = replacementCard;
-console.log(stack[6])
-
-//Task 3
+console.log(stack[6]);
+// Task 3
 let newCardTop = 25;
 stack.unshift(newCardTop);
-
-//Task 4
+console.log(stack);
+// Task 4
 position = 8;
-stack.slice(position, 1);
-
-//Task 5
-stack.shift(1);
-//console.log(stack)
-
-//Task 6
-let newCardBottom = 18;
-stack.push(newCardBottom);
-//console.log(stack)
-
-//Task 7
+stack.splice(position, 1);
+// splice takes in 2 arguments
+console.log(stack);
+// Task 5
+stack.shift();
+// shift will automatically take out position 0
+console.log(stack);
+// Task 6
+let newCardBotom = 18;
+stack.push(newCardBotom);
+console.log(stack);
+// Task 7
 stack.pop();
-console.log(stack)
-
-//Task 8
-let stackSize =5;
-let checkSizeOfStackResult = true;
-if(stackSize == stack.length){
-    checkSizeOfStackResult = true;}
-else{
-    checkSizeOfStackResult = false;}
-console.log(checkSizeOfStackResult)
+// if you call a list pop will take the last one out
+console.log(stack);
+// Task 8
+let stackSize = 9;
+// One way to check if its true or false:
+// let checkSizeOfStackResult
+// if(stackSize == stack.length){
+// checkSizeOfStackResult = true;}
+// else {
+//     checkSizeOfStackResult = false;
+// }
+// Another way to check:
+let checkSizeOfStackResult = stack.length == stackSize;
+console.log(checkSizeOfStackResult);
